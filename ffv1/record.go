4f05bb8d9115ef44fedd83dc2fa1879101ebc5af
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2019-10-18 16:40:39 +0100

    Move state initialization to record.go
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- ffv1/record.go
+++ ffv1/record.go
@@ -149,3 +149,24 @@ func parseConfigRecord(buf []byte, record *configRecord) error {
 
 	return nil
 }
+
+func (d *Decoder) initializeStates() {
+	for i := 1; i < 256; i++ {
+		d.state_transition[i] = uint8(int16(rangecoder.DefaultStateTransition[i]) + d.record.state_transition_delta[i])
+	}
+
+	d.initial_states = make([][][]uint8, len(d.record.initial_state_delta))
+	for i := 0; i < len(d.record.initial_state_delta); i++ {
+		d.initial_states[i] = make([][]uint8, len(d.record.initial_state_delta[i]))
+		for j := 0; j < len(d.record.initial_state_delta[i]); j++ {
+			d.initial_states[i][j] = make([]uint8, len(d.record.initial_state_delta[i][j]))
+			for k := 0; k < len(d.record.initial_state_delta[i][j]); k++ {
+				pred := int16(128)
+				if j != 0 {
+					pred = int16(d.initial_states[i][j-1][k])
+				}
+				d.initial_states[i][j][k] = uint8((pred + d.record.initial_state_delta[i][j][k]) & 255)
+			}
+		}
+	}
+}
